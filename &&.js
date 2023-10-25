bc7f4da239ce214c55947acd1cf8ee6c99d35c6e
Mari Mekhashishvili <103949541+kattamiso@users.noreply.github.com>
2023-10-25 15:47:10 +0400

    Create &&.js

---
+++ &&.js
@@ -0,0 +1,9 @@
+const tasty = (
+  <ul>
+    <li>Applesauce</li>
+    { !baby && <li>Pizza</li> }
+    { age > 15 && <li>Brussels Sprouts</li> }
+    { age > 20 && <li>Oysters</li> }
+    { age > 25 && <li>Grappa</li> }
+  </ul>
+);
